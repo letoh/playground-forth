aca2cac2e43633008de8bb432b9cbacc854bb3be
letoh <letoh.tw@gmail.com>
2014-05-09 16:48:50 +0800

    add new snippets of recursion

--- recursion.fs
+++ recursion.fs
@@ -12,11 +12,15 @@ foo
 : fib ( n -- m )
   dup 1 > if 1- dup recurse swap 1- recurse + then ;
 
+: fact ( n -- n! )
+  dup 1 > if dup 1- recurse * then ;
 
-: test ( n m -- )
-	?do i fib . cr loop ;
 
-10 1 test
+: test ( xt n m -- )
+	?do i over execute . cr loop drop ;
+
+' fib 10 1 test
+' fact 10 1 test
 
 bye
 
---
+++ recursion2.fs
@@ -0,0 +1,48 @@
+( mutural recursion )
+
+defer odd
+: even ( n -- f )
+	case
+	0 of true  endof
+	1 of false endof
+	1- odd dup
+	endcase
+;
+
+:noname ( n -- f )
+	case
+	1 of true  endof
+	0 of false endof
+	1- even dup
+	endcase
+; is odd
+
+
+0 [if]
+: >>> dup . ;
+: test ( n m -- )
+	?do
+		i >>> odd  ." odd  = " . cr
+		i >>> even ." even = " . cr
+	loop
+;
+
+[else]
+
+: (test) ( xt n -- )
+	dup . over execute
+	if ." -> " else ." >< " then >name .name ;
+
+: .tabstop 9 emit ;
+
+: test ( n m -- )
+	['] even -rot ['] odd  -rot
+	?do 2dup
+		i (test) .tabstop i (test) cr
+	loop 2drop
+;
+[then]
+
+3759 3750 test
+
+bye
